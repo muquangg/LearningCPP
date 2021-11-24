cf8cc492ef73012be79c2c08419fec8cc9efc292
muquangg <2607691426@qq.com>
2021-11-25 00:26:32 +0800

    modify author message

--- chapter_4/test_overload.cpp
+++ chapter_4/test_overload.cpp
@@ -7,14 +7,6 @@
  * @FilePath: \Learning_C++\chapter_4\test_overload.cpp
  */
 
-/*
- * @Author: your name
- * @Date: 2021-11-09 19:38:59
- * @LastEditTime: 2021-11-09 19:41:12
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \Learning_C++\chapter_4\test_override.cpp
- */
 #include <iostream> 
 #include <stdlib.h>
 #include <math.h>
